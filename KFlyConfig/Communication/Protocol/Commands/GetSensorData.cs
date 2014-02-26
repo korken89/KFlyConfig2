edffb41675b3c9339304eb796898ce2e0e5224a5
David Rosendahl <david.rosendahl@gmail.com>
2014-02-26 21:50:51 +0100

    GetSensorData implemented

---
+++ KFlyConfig/Communication/Protocol/Commands/GetSensorData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KFly.Communication
+{
+    /// <summary>
+    /// Same for 
+    /// GetRateControllerData
+    /// GetAttitudeControllerData
+    /// GetPositionControllerData
+    /// GetVelocityControllerData
+    /// </summary>
+    class GetSensorData : KFlyCommand
+    {
+
+        public SensorData Data;
+
+        public GetSensorData(KFlyCommandType getControllerType) : 
+            base(getControllerType)
+        {}
+
+        public override void ParseData(List<byte> data)
+        {
+            Data = SensorData.FromBytes(data);
+        }
+
+        public override List<Byte> ToTx()
+        {
+            if (Data == null)
+                return CreateTxWithHeader(new List<byte>());
+            else
+                return CreateTxWithHeader(Data.GetBytes());
+        }
+    }
+}
---
+++ KFlyConfig/DataClasses/SensorData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KFly.Communication
+{
+    public class SensorData
+    {
+        private XYZData _accelerometer = new XYZData();
+        private XYZData _gyro = new XYZData();
+        private XYZData _magnometer = new XYZData();
+        private float _pressure = 0f;
+
+        public XYZData Accelerometer
+        {
+            get
+            {
+                return _accelerometer;
+            }
+        }
+
+        public XYZData Gyro
+        {
+            get
+            {
+                return _gyro;
+            }
+        }
+
+        public XYZData Magnometer
+        {
+            get
+            {
+                return _magnometer;
+            }
+        }
+
+        public float Pressure
+        {
+            get
+            {
+                return _pressure;
+            }
+        }
+
+        public List<byte> GetBytes()
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(Accelerometer.GetBytes());
+            data.AddRange(Gyro.GetBytes());
+            data.AddRange(Magnometer.GetBytes());
+            data.AddRange(BitConverter.GetBytes(Pressure));
+            return data;
+        }
+
+        public void SetBytes(List<byte> bytes)
+        {
+            if (bytes.Count >= 40)
+            {
+                Accelerometer.SetBytes(bytes.GetRange(0, 12));
+                Gyro.SetBytes(bytes.GetRange(12, 12));
+                Magnometer.SetBytes(bytes.GetRange(24, 12));
+                _pressure = BitConverter.ToSingle(bytes.ToArray(), 36);
+            }
+        }
+        public static SensorData FromBytes(List<byte> bytes)
+        {
+            var c = new SensorData();
+            c.SetBytes(bytes);
+            return c;
+        }
+       
+    }
+}
